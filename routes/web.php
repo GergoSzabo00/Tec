e8205132ed7dc63e6e02f503e79c19957d681416
GergoSzabo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-04-15 13:44:37 +0200

    added admin dashboard and product creation pages

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,12 @@ use App\Http\Controllers\Controller;
 
 Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
 
+Route::middleware('admin')->group(function () 
+{
+    Route::get('admin', [Controllers\Admin\AdminController::class, 'index'])->name('admin');
+    Route::get('admin/products/create', [Controllers\Admin\ProductController::class, 'create'])->name('product.create');
+    Route::post('admin/products/create', [Controllers\Admin\ProductController::class, 'store']);
+    Route::get('admin/products/{product}', [Controllers\Admin\ProductController::class, 'show'])->name('product.details');
+});
+
 require __DIR__ . '/auth.php';
