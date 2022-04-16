01273eef44f27a74ded7aefa095a703c5a79ff31
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-04-16 03:38:04 +0200

    added statistics from database

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     function index()
     {
-        return view('admin.admin');
+        $total_revenue = Order::join('order_status', 'orders.order_status', '=', 'order_status.id')
+        ->where('order_status.name', 'Completed')->sum('total_price');
+        $total_orders = Order::count();
+        $registered_users = User::count();
+        return view('admin.admin')->with(compact('total_revenue', 'total_orders', 'registered_users'));
     }
 }
