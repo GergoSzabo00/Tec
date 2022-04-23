599b9edc88661cab3061de0d5c460d1aafe84ef7
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-04-23 15:07:29 +0200

    added manufacturer to fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = [
-        'product_name', 'description', 'price', 'quantity_in_stock'
+        'product_name','description', 'manufacturer', 'price', 'quantity_in_stock'
     ];
 
     public function manufacturer()
