9d1ea72f8b532011e28df48b5634921362452018
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-04-23 18:43:52 +0200

    Specified pivot table name

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,7 +15,7 @@ class Category extends Model
 
     public function products()
     {
-        return $this->belongsToMany(Product::class);
+        return $this->belongsToMany(Product::class, 'product_categories');
     }
 
 }
