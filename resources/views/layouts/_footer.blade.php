a72a837a02486667635f65fdd84c658986d13929
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-04-24 18:29:41 +0200

    removed contact info and payment info from footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,40 +1,5 @@
 <footer class="bg-dark text-center text-white">
-    <div class="container">
-        <div class="row p-3">
-            <div class="col-lg-6 mb-3 mb-lg-0">
-                <h4>{{ __('Contact') }}</h4>
-                <div class="contact-item-holder">
-                    <div class="contact-item">
-                        <div class="info-circle">
-                            <i class="fa-solid fa-phone"></i>
-                        </div>
-                        <h6 class="ms-3">+36123456789</h6>
-                    </div>
-                    <div class="contact-item">
-                        <div class="info-circle">
-                            <i class="fa-solid fa-location-dot"></i>
-                        </div>
-                        <h6 class="ms-3">Fictional Street</h6>
-                    </div>
-                    <div class="contact-item">
-                        <div class="info-circle">
-                            <i class="fa-solid fa-envelope"></i>
-                        </div>
-                        <h6 class="ms-3">support@techzone.com</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 mb-3 mb-lg-0">
-                <h4>{{ __('Payment information') }}</h4>
-                <p>{{ __('Shipping cost:') }} </p>
-                <p>{{ __('Accepted payment methods:') }}</p>
-                <div class="payment-methods">
-                    <i class="fa-brands fa-cc-mastercard"></i>
-                    <i class="fa-solid fa-money-bill-1"></i>
-                    <i class="fa-brands fa-cc-visa"></i>
-                </div>
-            </div>
-        </div>
+    <div class="container p-3">
         <div class="social-holder">
             <a href="#" class="social social-facebook">
                 <i class="fa-brands fa-facebook-f"></i>
@@ -46,7 +11,7 @@
                 <i class="fa-brands fa-twitter"></i>
             </a>
         </div>
-        <div class="text-center p-3">
+        <div class="text-center mt-2">
             Copyright &copy; {{ now()->year  }} {{ config('app.name') }}
         </div>
     </div>
