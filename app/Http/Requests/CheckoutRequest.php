a84422ea3b2f3f18cfe7e72d9a88f4ec172caca8
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-05-05 00:42:05 +0200

    updated checkout request

--- app/Http/Requests/CheckoutRequest.php
+++ app/Http/Requests/CheckoutRequest.php
@@ -13,17 +13,34 @@ class CheckoutRequest extends FormRequest
      */
     public function rules()
     {
+        if(auth()->check())
+        {
+            return [
+                'firstname' => 'required|alpha|min:2|max:255',
+                'lastname' => 'required|alpha|min:2|max:255',
+                'email' => 'required|string|email|max:255',
+                'phone' => 'required|min:4|max:15',
+                'payment_option' => 'required|exists:payment_options,id',
+                'addresses' => 'required|check_existence_or_other:addresses,id,newAddress',
+                'newAddressCountry' => 'required_if:addresses,newAddress|exclude_unless:addresses,newAddress|exists:countries,id',
+                'newAddressCity' => 'required_if:addresses,newAddress|exclude_unless:addresses,newAddress|alpha|min:2|max:255',
+                'newAddressState' => 'required_if:addresses,newAddress|exclude_unless:addresses,newAddress|alpha|min:2|max:255',
+                'newAddressZip_code' => 'required_if:addresses,newAddress|exclude_unless:addresses,newAddress|integer|min:0|digits_between:3,10',
+                'newAddressAddress' => 'required_if:addresses,newAddress|exclude_unless:addresses,newAddress|string|min:2|max:255',
+                'save_address' => 'boolean'
+            ];
+        }
         return [
             'firstname' => 'required|alpha|min:2|max:255',
             'lastname' => 'required|alpha|min:2|max:255',
             'email' => 'required|string|email|max:255',
+            'phone' => 'required|min:4|max:15',
             'country' => 'required|exists:countries,id',
             'city' => 'required|alpha|min:2|max:255',
             'state' => 'required|alpha|min:2|max:255',
             'zip_code' => 'required|integer|min:0|digits_between:3,10',
             'address' => 'required|string|min:2|max:255',
-            'phone' => 'required|min:4|max:15',
-            'payment_option' => 'required|exists:payment_options,id'
+            'payment_option' => 'required|exists:payment_options,id',
         ];
     }
 
@@ -34,7 +51,10 @@ class CheckoutRequest extends FormRequest
             'lastname' => trcw($this->lastname),
             'city' => trcw($this->city),
             'state' => trcw($this->state),
-            'address' => trcw($this->address)
+            'address' => trcw($this->address),
+            'newAddressCity' => trcw($this->newAddressCity),
+            'newAddressState' => trcw($this->newAddressState),
+            'newAddressAddress' => trcw($this->newAddressAddress)
         ]);
     }
 
