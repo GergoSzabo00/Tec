632a2adcf34b1a9029ad78817ab63967d371b81c
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-05-15 22:17:24 +0200

    added hungarian localization files

--- lang/hu/auth.php
+++ lang/hu/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'Invalid email or password.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Érvénytelen email vagy jelszó.',
+    'password' => 'A megadott jelszó érvénytelen.',
+    'throttle' => 'Túl sok bejelentkezési kísérlet. Kérlek próbáld újra :seconds másodperc múlva.',
 
 ];
