35fd412930f613a0acabf1ecc9e1196605176fac
Gergő Szabó <YGJWWN@std.uni-eszterhazy.hu>
2023-10-16 16:15:09 +0200

    created coupon request validation

--- app/Http/Requests/CouponRequest.php
+++ app/Http/Requests/CouponRequest.php
@@ -14,7 +14,11 @@ class CouponRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'code' => 'required|alpha_num|unique:coupons|min:3|max:255',
+            'type' => 'required|in:numeric,percentage',
+            'value' => 'required|numeric|between:0,99999999.99',
+            'minimum_cart_amount' => 'numeric|between:0,99999999.99',
+            'expires_at' => 'required|date'
         ];
     }
 }
