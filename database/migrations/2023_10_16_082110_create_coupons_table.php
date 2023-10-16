22dbd7d60312e56a0dec4056357ec96a7f5fb5db
Gergő Szabó <YGJWWN@std.uni-eszterhazy.hu>
2023-10-16 11:19:49 +0200

    added coupons table migration

---
+++ database/migrations/2023_10_16_082110_create_coupons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->enum('type', ['numeric', 'percentage']);
+            $table->decimal('amount', 10, 2);
+            $table->decimal('minimal_cart_amount', 10, 2);
+            $table->date('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
