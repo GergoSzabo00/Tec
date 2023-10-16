f34679e5410e12790d1c18eaececbb9912e72ab7
Gergő Szabó <YGJWWN@std.uni-eszterhazy.hu>
2023-10-16 13:37:02 +0200

    created coupon model

---
+++ app/Models/Coupon.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name', 'type', 'amount', 'minimal_cart_amount', 'expires_at'
+    ];
+
+}
