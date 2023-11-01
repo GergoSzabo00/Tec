1a79d22c9a132f0182325629626b8cd9479075cf
Gergő Szabó <YGJWWN@std.uni-eszterhazy.hu>
2023-11-01 19:52:08 +0100

    added default expires at date to tomorrow

--- resources/views/admin/coupons/create.blade.php
+++ resources/views/admin/coupons/create.blade.php
@@ -99,7 +99,7 @@
                         </div>
                         <x-forms.input type="number" id="coupon_value" name="coupon_value" icon="dollar" label="{{ __('Value') }}" placeholder="{{ __('Value') }}" min=".01" max="99999999.99" step=".01" />
                         <x-forms.input type="number" id="minimum_cart_amount" name="minimum_cart_amount" icon="dollar" label="{{ __('Minimum cart amount') }}" placeholder="{{ __('Minimum cart amount') }}" value="0" min="0" max="99999999.99" step=".01" />
-                        <x-forms.input type="date" id="expires_at" name="expires_at" label="{{ __('Expires at') }}" placeholder="{{ __('Expires at') }}" />
+                        <x-forms.input type="date" id="expires_at" name="expires_at" label="{{ __('Expires at') }}" placeholder="{{ __('Expires at') }}" value="{{ now()->add(new DateInterval('P1D'))->toDateString() }}" />
                     </div>
                 </div>
             </div>
