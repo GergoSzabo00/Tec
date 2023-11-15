6a00d4fd2ee53d8f1f80b7e7347bc034ef4b3d62
GergoSzabo00 <YGJWWN@std.uni-eszterhazy.com>
2023-11-15 14:27:53 +0100

    adatvédelmi nyilatkozat

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -5,5 +5,120 @@
     <div class="d-flex align-items-center">
         <h2>{{__('Privacy Policy')}}</h2>
     </div>
+
+    <div class="p-3">
+        <strong>A. Bevezetés</strong><p></p>
+        <ol>
+        <li>Nagyon fontos számunkra a weboldalunk látogatói személyes adatainak védelme, és elkötelezettek vagyunk ennek megfelelő biztosítása mellett. Ez az adatvédelmi nyilatkozat tájékoztatást nyújt arról, hogy hogyan kezeljük az Ön személyes adatait.</li>
+        <li>Ha hozzájárul a sütik használatához a jelen adatvédelmi nyilatkozat feltételeinek megfelelően, amikor először látogatja meg weboldalunkat, azzal engedélyezi számunkra a sütik használatát minden egyes alkalommal, amikor Ön meglátogatja weboldalunkat.</li>
+        </ol>
+        <strong>B. Forrás</strong><br>
+        Ez a dokumentum a SEQ Legal (seqlegal.com) sablonján alapszik, amelyet a Website Planet módosított (<a href="https://www.websiteplanet.com">www.websiteplanet.com</a>)<p></p>
+        <strong>C. A személyes adatok gyűjtése</strong><p></p>
+        A következő típusú személyes adatokat gyűjthetjük, tárolhatjuk és felhasználhatjuk:<p></p>
+        <ol>
+        <li>a számítógépével kapcsolatos információkat, beleértve az Ön IP-címét, a böngészője típusát és verzióját, valamint az operációs rendszerét;</li>
+        <li>a weboldalon a navigáció adatait;</li>
+        <li>a weboldalunkon a regisztrációkor megadott információkat, például az e-mail címét;</li>
+        <li>a weboldalunkon a profil létrehozásakor megadott információkat — például: a neve, telefonszáma;</li>
+        <li>a weboldalunkon a szolgáltatásaink igénybevételekor megadott információkat;</li>
+        <li>az Ön által vásárolt szolgáltatásokkal, az Ön által használt szolgáltatásokkal vagy a weboldalunkon keresztül végzett tranzakciókkal kapcsolatos információkat, többek között: a neve, címe, telefonszáma, e-mail címe és a bankkártyája adatai;</li>
+        <li>az e-mailben vagy a weboldalunkon keresztül az Ön által elküldött információkat, beleértve azok tartalmát és metaadatait;</li>
+        <li>minden egyéb személyes információt, amelyet elküldött számunkra.</li>
+        </ol>
+        Mielőtt valamely másik személy személyes adatait feltárná előttünk, Önnek kötelező az adott személy hozzájárulásával rendelkeznie a személyes adatok megosztását valamint a személyes adatoknak a jelen adatvédelmi nyilatkozat előírásaival összhangban történő feldolgozását illetően.<p></p>
+        <strong>D. Az Ön személyes adatainak felhasználása</strong><p></p>
+        A weboldalunkon keresztül számunkra elküldött személyes adatokat a jelen adatvédelmi nyilatkozatban vagy a weboldal vonatkozó oldalain meghatározott célokra használjuk fel. Az Ön személyes adatait a következő célokra használhatjuk fel:<p></p>
+        <ol>
+        <li>a weboldalunk és a vállalkozásunk adminisztrálása;</li>
+        <li>a weboldalunk testreszabása az Ön számára;</li>
+        <li>a weboldalunkon található szolgáltatások elérhetővé tétele az Ön számára</li>
+        <li>a weboldalunkon keresztül megvásárolt áruk postázása az Ön részére;</li>
+        <li>a weboldalunkon keresztül megvásárolt szolgáltatások nyújtása az Ön részére;</li>
+        <li>számlák és fizetési emlékeztetők küldése, és Öntől a befizetések begyűjtése;</li>
+        <li>nem marketing célú kereskedelmi kommunikáció küldése az Ön részére;</li>
+        <li>a weboldalunkkal kapcsolatos, az Ön által vagy az Önről tett tudakozódások és panaszok kezelése;</li>
+        <li>a weboldalunk biztonságának megőrzése és a csalások megelőzése;</li>
+        <li>a weboldalunk használatát szabályozó feltételek betartásának ellenőrzése; valamint</li>
+        <li>egyéb felhasználások.</li>
+        </ol>
+        Az Ön kifejezett hozzájárulása nélkül nem adjuk át az Ön személyes adatait harmadik feleknek ezen harmadik felek vagy más harmadik felek által folytatott közvetlen marketing céljából.<p></p>
+        <strong>E. A személyes adatok közzététele</strong><p></p>
+        A személyes adatait bármilyen alkalmazottunkkal, tisztségviselőnkkel, biztosító társasággal, szakmai tanácsadókkal, ügynökökkel, beszállítókkal vagy alvállalkozókkal közölhetjük, amennyiben ez ésszerű és szükséges a jelen adatvédelmi nyilatkozatban meghatározott célok biztosításához.<p></p>
+        A személyes adatait közölhetjük a cégcsoportunk bármelyik tagjával (azaz leányvállalatainkkal, a holdingtársaságunkkal és annak minden leányvállalataival), amennyiben ez ésszerű és szükséges a jelen adatvédelmi nyilatkozatban meghatározott célok biztosításához.<p></p>
+        A személyes adatait közzétehetjük:<p></p>
+        <ol>
+        <li>olyan mértékben, amennyire a törvény ezt megköveteli;</li>
+        <li>a folyamatban lévő vagy jövőbeni bírósági eljárásokkal kapcsolatban;</li>
+        <li>a törvényes jogaink megállapítása, gyakorlása vagy védelme érdekében (többek között: a csalások megelőzése és a hitelkockázat csökkentése céljából történő információszolgáltatás mások számára);</li>
+        <li>az általunk eladott (vagy a jövőben eladott) minden vállalkozás vagy vagyontárgy vevője számára; és</li>
+        <li>bármely olyan személy számára, akinek az esetében ésszerűen feltételezhetjük, hogy bírósághoz vagy más illetékes hatósághoz fordulhat a személyes információ közzététele érdekében, amennyiben ésszerű véleményünk szerint az ilyen bíróság vagy hatóság valószínűleg elrendelné az említett személyes információ közzétételét.</li>
+        </ol>
+        A jelen adatvédelmi nyilatkozatban foglalt esetek kivételével az Ön személyes adatait nem adjuk át harmadik feleknek.<p></p>
+        <strong>F. A személyes adatok megőrzése</strong><p></p>
+        <ol>
+        <li>A jelen F. szakasz tájékoztat az adatmegőrzési irányelveinkről és eljárásainkról, amelyek célja annak biztosítása, hogy a személyes adatok megőrzésére és törlésére vonatkozó jogi kötelezettségeinknek megfelelően járjunk el.</li>
+        <li>A személyes adatokat, amelyeket bármilyen célból vagy célokból feldolgozunk, tilos annál hosszabb ideig megőrizni, mint amennyi ideig szükséges a tárolásuk ehhez a célhoz vagy célokhoz.</li>
+        <ol type="a">
+        </ol>
+        </li>
+        <li>A F. szakasz egyéb rendelkezéseinek sérelme nélkül, a személyes adatokat tartalmazó dokumentumokat (beleértve az elektronikus dokumentumokat is) az alábbiaknak megfelelően megőrizzük:
+        <ol type="a">
+        <li>olyan mértékben, amennyire a törvény ezt megköveteli;</li>
+        <li>amennyiben a dokumentumok relevánsak lehetnek bármilyen folyamatban lévő vagy jövőbeni bírósági eljárásban; és</li>
+        <li>törvényes jogaink megállapítása, gyakorlása vagy védelme érdekében (többek között: a csalások megelőzése és a hitelkockázat csökkentése céljából történő információszolgáltatás mások számára).</li>
+        </ol>
+        </li>
+        </ol>
+        <strong>G. Az Ön személyes adatainak biztonsága</strong><p></p>
+        <ol>
+        <li>Megtesszük a megfelelő technikai és szervezeti óvintézkedéseket annak érdekében, hogy megakadályozzuk a személyes adatai elvesztését, a velük való visszaélést vagy a módosításukat.</li>
+        <li>Minden, az Ön által megadott személyes információt biztonságos (jelszóval és tűzfallal védett) szervereinken tároljuk.</li>
+        <li>A weboldalunkon keresztül lebonyolított összes elektronikus pénzügyi tranzakciót titkosítással védjük.</li>
+        <li>Ön tudomásul veszi, hogy az információ továbbítása az interneten keresztül alapvetően bizonytalan jellegű, és nem garantálhatjuk az interneten keresztül továbbított adatok biztonságát.</li>
+        <li>Ön felelős a weboldalunk eléréséhez használt jelszava bizalmas kezeléséért; nem kérjük el a jelszavát (kivéve, amikor bejelentkezik weboldalunkra).</li>
+        </ol>
+        <strong>H. Módosítások</strong><p></p>
+        Időnként frissíthetjük ezt az adatvédelmi nyilatkozatot azáltal, hogy közzétesszük az új verzióját a weboldalunkon. Időnként ellenőrizze ezt az oldalt annak érdekében, hogy megismerje az adatvédelmi nyilatkozat módosításait. Önt az adatvédelmi nyilatkozat változásairól e-mailben vagy a weboldalunkon található privát üzenetküldő rendszeren keresztül értesíthetjük.<p></p>
+        <strong>I. Az Ön jogai</strong><p></p>
+        Felszólíthat minket arra, hogy bocsássunk rendelkezésére minden személyes információt, amely nálunk elérhető Önről; az ilyen információk szolgáltatása a következő feltételek mellett történik:<p></p>
+        <ol>
+        <li>az Ön személyazonosságának a megfelelő módon történő igazolása (ebből a célból; általában elfogadjuk útlevelének fénymásolatát, amelyet közjegyző hitelesít, közüzemi számla olyan eredeti példányával együtt, amely tartalmazza az Ön jelenlegi lakcímét).</li>
+        </ol>
+        A törvény által megengedett mértékben visszatarthatjuk az Ön által kért személyes információkat.<p></p>
+        Bármikor felszólíthat bennünket arra, hogy marketing célból ne dolgozzuk fel személyes adatait.<p></p>
+        A gyakorlatban Ön általában előzőleg kifejezetten egyetért azzal, hogy személyes adatait felhasználhatjuk marketing célokra, vagy pedig lehetőséget biztosítunk az Ön számára azt illetően, hogy visszautasíthassa a személyes adatai marketing célokra történő felhasználását.<p></p>
+        <strong>L. Az adatok frissítése</strong><p></p>
+        Kérjük, tudassa velünk, ha javítani vagy frissíteni kell az Önnel kapcsolatosan általunk tárolt személyes adatokat.<p></p>
+        <strong>M. Sütik</strong><p></p>
+        Weboldalunk sütiket alkalmaz. A süti egy azonosítót (betűket és számokat) tartalmazó fájlnak felel meg, amelyet a webszerver küld a webböngészőnek, és amelyet a böngésző eltárol. Ezután megtörténik az azonosító visszaküldése a szerverhez minden alkalommal, amikor a böngésző lekér egy adott oldalt a szerverről. A sütik lehetnek „tartós” vagy „munkamenet” sütik: a tartós sütiket a böngésző tárolja, és a megadott lejárati időpontig érvényben maradnak, kivéve, ha a felhasználó a lejárati idő előtt törli őket; a munkamenet süti ezzel szemben lejár a felhasználói munkamenet végén, amikor a böngészőt bezárja a felhasználó. A sütik általában nem tartalmaznak olyan információt, amely által személyesen azonosítható lenne a felhasználó, azonban az Önről általunk tárolt személyes adatok kapcsolódhatnak a sütikben tárolt illetve a sütikből kiolvasható információkhoz. Weboldalunkon munkamenet sütiket és tartós sütiket is használunk.<p></p>
+        <ol>
+        <li>Az általunk a weboldalunkon alkalmazott sütik nevét, valamint azok felhasználási céljait az alábbiakban ismertetjük:
+        <ol type="a">
+            <li>techzone_session: bejelenetkezett felhasználó navigációja, a kosár használatának lehetősége;</li>
+            <li>XSRF-TOKEN: a csalások megakadályozása és a weboldal biztonságának javítása;</li>
+            <li>locale: felhasználói felület nyelvének testreszabása;</li>
+        </ol>
+        </li>
+        <li>A legtöbb böngészőben beállítható a sütik elfogadásának megtagadása — például:
+        <ol type="a">
+        <li>az Internet Explorer böngészőben (10. verziószám) letilthatja a sütiket a sütikezelés felülbírálását érintő beállításokban, ehhez kattintson a következőkre: „Eszközök”, „Internetbeállítások”, „Adatvédelem”, végül pedig a „Speciális” opcióra;</li>
+        <li>A Firefox böngészőben (24. verziószám) az összes süti letiltásához kattintson a következőkre: „Eszközök”, „Opciók”, „Adatvédelem”, ezután a legördülő menüből válassza az „Egyéni beállítások használata előzményekhez” opciót, és törölje a pipát a „Sütik elfogadása” opciónál; valamint</li>
+        <li>A Chrome böngészőben (29. verziószám) az összes süti letiltásához válassza a „Testreszabás és vezérlés” menüt, ezután kattintson a következőkre: „Beállítások”, „Speciális beállítások megjelenítése”, „Tartalombeállítások”, végül válassza ki a „Adatmentés tiltása a webhelyeken” opciót a „Cookie-k” résznél.</li>
+        </ol>
+        </li>
+        </ol>
+        Ha letiltja az összes süti használatát, akkor sok weboldal használhatósága romlani fog. A weboldalunkon pedig egyes funkciók nem lesznek elérhetők az Ön számára.<p></p>
+        <ol start="3">
+        <li>Törölheti a számítógépén már tárolt sütiket is — például:
+        <ol type="a">
+        <li>az Internet Explorer böngészőben (10. verziószám) manuálisan kell törölnie a süti-fájlokat (ehhez a <a href="http://support.microsoft.com/kb/278835" target="_blank" rel="noopener nofollow noreferrer">http://support.microsoft.com/kb/278835</a> címen talál leírást);</li>
+        <li>A Firefox böngészőben (24. verziószám) a sütik törléséhez kattintson a következőkre: „Eszközök”, „Opciók”, „Adatvédelem”, ezután válassza az „Egyéni beállításokat használ az előzményekhez” opciót, majd kattintson a „Sütik megjelenítése” opcióra, végül az „Összes eltávolítása” elemre; valamint</li>
+        <li>A Chrome böngészőben (29. verziószám) az összes süti törléséhez válassza a „Testreszabás és vezérlés” menüt, ezután kattintson a következőkre: „Beállítások”, „Speciális beállítások megjelenítése”, „Böngészési adatok törlése”, utóbbinál pedig válassza az „Összes cookie és webhelyadat” lehetőséget, mielőtt a „ Böngészési adatok törlése” elemre kattint.</li>
+        </ol>
+        </li>
+        <li>Ha törli a sütiket, akkor sok weboldal használhatósága romlani fog.</li>
+        </ol>
+        </div>
 </div>
 @endsection
